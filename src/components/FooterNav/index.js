c5669ade5cd8f8fc98acb5f5b47810bf35cbd994
Sam <sambyoung@gmail.com>
2020-03-18 17:07:59 -0400

    made footer nav into its own component

---
+++ src/components/FooterNav/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import * as ROUTES from '../../constants/routes';
+const FooterNav = () => (
+	<footer>
+		<div>
+		<a
+			href="https://docs.google.com/document/d/1q4BHgMRKO3W6-gRpGPB6EFXFfvyDLGaCsj7kny7CTG4/edit?usp=sharing"
+			target="_blank"
+		>
+			about the leveler
+		</a>
+		</div>
+		<div>
+		<a
+			href="https://docs.google.com/document/d/1OqjFn7f5YRxzD71v-ieGSruQUdTST5K3woPl-yrDDkc/edit?usp=sharing"
+			target="_blank"
+		>
+			updates
+		</a>
+		</div>
+		<div>
+		<a
+			href="https://docs.google.com/forms/d/1pYA60eG7qG4ekLWf7kEUwJuwJnS6sc8KAH_RPdawvJU/edit#responses"
+			target="_blank"
+		>
+			join the database
+		</a>
+		</div>
+		<div>
+		<a
+			href="https://docs.google.com/document/d/1zyTKTN55fOQHiB5XKtkIVu42_oCtSq7WbFAsLB4blD8/edit?usp=sharing"
+			target="_blank"
+		>
+			contribute
+		</a>
+		</div>
+	</footer>
+);
+export default FooterNav;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { withFirebase } from '../Firebase';
 import { SHEET_DB } from '../../constants/routes'
+import FooterNav from '../FooterNav';
 import ReactGA from 'react-ga';
 
 const HomePage = () => (
@@ -120,41 +121,9 @@ class HomeLandingBase extends Component {
 				<button onClick={() => { this.sendToSheet(this.state) }}>distribute</button>
 			  </div>
 			</section>
-			<footer>
-			  <div>
-				<a
-				  href="https://docs.google.com/document/d/1q4BHgMRKO3W6-gRpGPB6EFXFfvyDLGaCsj7kny7CTG4/edit?usp=sharing"
-				  target="_blank"
-				>
-				  about the leveler
-				</a>
-			  </div>
-			  <div>
-				<a
-				  href="https://docs.google.com/document/d/1OqjFn7f5YRxzD71v-ieGSruQUdTST5K3woPl-yrDDkc/edit?usp=sharing"
-				  target="_blank"
-				>
-				  updates
-				</a>
-			  </div>
-			  <div>
-				<a
-				  href="https://docs.google.com/forms/d/1pYA60eG7qG4ekLWf7kEUwJuwJnS6sc8KAH_RPdawvJU/edit#responses"
-				  target="_blank"
-				>
-				  join the database
-				</a>
-			  </div>
-			  <div>
-				<a
-				  href="https://docs.google.com/document/d/1zyTKTN55fOQHiB5XKtkIVu42_oCtSq7WbFAsLB4blD8/edit?usp=sharing"
-				  target="_blank"
-				>
-				  contribute
-				</a>
-			  </div>
-			</footer>
-
+			
+			<FooterNav />
+			
 		  </div>
     );
   }
