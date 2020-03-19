e252bf647ac98bc82b96ec5c17230897a5466a66
Adam Levy <adam@Adams-MacBook-Pro.local>
2020-03-19 13:50:58 -0400

    add react-autocomplete and configure form

---
+++ src/components/SignUp/FormikPlacesAutocomplete.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import PlacesAutocomplete from "react-places-autocomplete";
+
+class FormikPlacesAutoComplete extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: props.field.name,
+      location: props.value || ""
+    };
+  }
+
+  handleError = error => {
+    this.props.form.setFieldError(this.state.name, error);
+  };
+
+  handleChange = location => {
+    this.setState(() => {
+      this.props.form.setFieldTouched(`${this.state.name}.value`);
+      this.props.form.setFieldTouched(`${this.state.name}.location`);
+      this.props.form.setFieldValue(this.state.name, { value: location });
+      return { location };
+    });
+  };
+
+  handleSelect = location => {
+    this.setState(() => {
+      this.props.form.setFieldValue(this.state.name, location)
+    });
+  };
+
+  render() {
+    const {
+      field: { name, ...field }, // { name, value, onChange, onBlur }
+      form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+      classes,
+      label,
+      ...props
+    } = this.props;
+
+    const error = errors[name];
+    const touch = touched[name];
+    const searchOptions = {
+      types: ['(cities)']
+     }
+    return (
+      <PlacesAutocomplete
+        name={name}
+        id={name}
+        {...props}
+        value={this.state.location}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+        onError={this.handleError}
+        searchOptions={searchOptions}
+        shouldFetchSuggestions={this.state.location.length > 2}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: "Search Places ...",
+                className: "location-search-input form-control"
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? "suggestion-item--active"
+                  : "suggestion-item";
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                  : { backgroundColor: "#ffffff", cursor: "pointer" };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    );
+  }
+}
+
+export default FormikPlacesAutoComplete;
