c79dcd63cae971ed168c289f2ce61168c9021ba3
Adam Levy <adam@Adams-MacBook-Pro.local>
2020-03-19 13:50:58 -0400

    add react-autocomplete and configure form

--- src/components/SignUp/registration.js
+++ src/components/SignUp/registration.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Field, Formik } from "formik";
 import * as Yup from "yup";
+import FormikPlacesAutocomplete from "./FormikPlacesAutocomplete.jsx";
 
 const validationSchema = Yup.object({
     email: Yup.string().email().required("Required"),
-    city: Yup.string().required("Required"),
-    state_or_country: Yup.string().min(1).required("Required"),
     industry: Yup.string().min(1),
     paypal: Yup.string().url(),
     venmo: Yup.string().url(),
@@ -19,8 +18,6 @@ const Registration = (props) => (
       initialValues={{ 
           email: "", 
           timestamp: "", 
-          city: "",
-          state_or_country: "", 
           industry: "",
           description: "",
           suggestion: ""
@@ -47,69 +44,8 @@ const Registration = (props) => (
               name="email"
             />
             {errors.email}
-            <label htmlFor="city">City</label>
-            <input
-              type="text"
-              onChange={handleChange}
-              value={values.city}
-              name="city"
-            />
-            {errors.city}
-            <label htmlFor="state_or_country">State (or Country)</label>
-            <Field as="select" name="state_or_country">
-              <option value="">----</option>
-              <option value="AL">Alabama</option>
-	            <option value="AK">Alaska</option>
-	            <option value="AZ">Arizona</option>
-	            <option value="AR">Arkansas</option>
-	            <option value="CA">California</option>
-	            <option value="CO">Colorado</option>
-	            <option value="CT">Connecticut</option>
-	            <option value="DE">Delaware</option>
-	            <option value="DC">District of Columbia</option>
-	            <option value="FL">Florida</option>
-	            <option value="GA">Georgia</option>
-	            <option value="HI">Hawaii</option>
-	            <option value="ID">Idaho</option>
-	            <option value="IL">Illinois</option>
-	            <option value="IN">Indiana</option>
-	            <option value="IA">Iowa</option>
-	            <option value="KS">Kansas</option>
-	            <option value="KY">Kentucky</option>
-	            <option value="LA">Louisiana</option>
-	            <option value="ME">Maine</option>
-	            <option value="MD">Maryland</option>
-	            <option value="MA">Massachusetts</option>
-	            <option value="MI">Michigan</option>
-	            <option value="MN">Minnesota</option>
-	            <option value="MS">Mississippi</option>
-	            <option value="MO">Missouri</option>
-	            <option value="MT">Montana</option>
-	            <option value="NE">Nebraska</option>
-	            <option value="NV">Nevada</option>
-	            <option value="NH">New Hampshire</option>
-	            <option value="NJ">New Jersey</option>
-	            <option value="NM">New Mexico</option>
-	            <option value="NY">New York</option>
-	            <option value="NC">North Carolina</option>
-	            <option value="ND">North Dakota</option>
-	            <option value="OH">Ohio</option>
-	            <option value="OK">Oklahoma</option>
-	            <option value="OR">Oregon</option>
-	            <option value="PA">Pennsylvania</option>
-	            <option value="RI">Rhode Island</option>
-	            <option value="SC">South Carolina</option>
-	            <option value="SD">South Dakota</option>
-	            <option value="TN">Tennessee</option>
-	            <option value="TX">Texas</option>
-	            <option value="UT">Utah</option>
-	            <option value="VT">Vermont</option>
-	            <option value="VA">Virginia</option>
-	            <option value="WA">Washington</option>
-	            <option value="WV">West Virginia</option>
-	            <option value="WI">Wisconsin</option>
-	            <option value="WY">Wyoming</option>
-            </Field>
+            <label htmlFor="location">Location</label>
+            <Field name="location" component={FormikPlacesAutocomplete} />
             <label>Industry</label>
             <Field as="select" name="industry">
               <option value="">----</option>
