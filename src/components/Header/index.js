8b0801e5478218f19f41132d55818e75a7cdc8b4
Sam <sambyoung@gmail.com>
2020-03-21 00:45:18 -0400

    added link to header logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
 	<header>
-		<img src="./leveler-logo.png" alt="Logo img" />
+		<Link to='/'>
+			<img src="./leveler-logo.png" alt="Logo img" />
+		</Link>
 		<p className="top">peer to peer wealth distribution</p>
 	</header>
 )
