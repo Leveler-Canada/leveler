1f69db347f6bc4ddf440e49de91a0f8f1c3596f7
Sam <sambyoung@gmail.com>
2020-03-21 22:40:41 -0400

    changed copy in DistributeHeader.js

--- src/components/Distribute/DistributeHeader.js
+++ src/components/Distribute/DistributeHeader.js
@@ -2,10 +2,7 @@ import React from 'react';
 
  const DistributeHeader = props => (
 	<div>
-		<p class="distribute-header-main"><b>How it works. SHOULD BE DONE ON A MOBILE DEVICE:</b></p>
-		<p class="distribute-header"><b>You've been assigned a group of 10 people in need.</b></p>
-		<p class="distribute-header"><b>Pay each of the people in your assigned group an equal amount of money</b></p>
-		<p class="distribute-header"><b>Put <i>"leveler"</i> in the memo. Click 'I contributed' to confirm and move on to the next person'</b></p>
+		<p class="distribute-header-main"><b>The leveler is a database of freelancers, service industry and gig economy folks currently without income due to COVID-19.</b></p>
 		<p class="distribute-header">Below are 10 people <b><i>chosen at random</i></b>, send the <b><i>same amount</i></b> to each person</p>
 	</div>
 )
