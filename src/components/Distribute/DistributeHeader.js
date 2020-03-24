36c8f5030098c42e5facf0634ad960462a4e0055
Adam Levy <alevy110@bloomberg.net>
2020-03-24 01:42:36 -0400

    clearing warnings

--- src/components/Distribute/DistributeHeader.js
+++ src/components/Distribute/DistributeHeader.js
@@ -2,8 +2,8 @@ import React from 'react';
 
  const DistributeHeader = props => (
 	<div>
-		<p class="distribute-header-main"><b>The leveler is a database of freelancers, service industry and gig economy folks currently without income due to COVID-19.</b></p>
-		<p class="distribute-header">Below are 10 people <b><i>chosen at random</i></b>, send the <b><i>same amount</i></b> to each person</p>
+		<p className="distribute-header-main"><b>The leveler is a database of freelancers, service industry and gig economy folks currently without income due to COVID-19.</b></p>
+		<p className="distribute-header">Below are 10 people <b><i>chosen at random</i></b>, send the <b><i>same amount</i></b> to each person</p>
 	</div>
 )
 
