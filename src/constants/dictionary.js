1e5c1c0aa32bbf0f97e3f1ee1d8a1a2dcba171ea
oscar <oscar@worky.mx>
2020-04-08 10:02:52 -0500

    ref(languague): Intial translation package

---
+++ src/constants/dictionary.js
@@ -0,0 +1,32 @@
+const localizationBundle = {
+    'app.button.Submit': 'Enviar',
+    'distribute': 'Distribuye',
+    'recive': 'Recibe',
+
+    intro: {
+        main: 'Leveler es una herramienta para que la población económicamente estable pueda ayudar a trabajadorxs independientes \n' +
+            'que han perdido trabajo o han sido afectados económicamente por la pandemia',
+        distribute: 'Haz click en "Distribuye" y leveler seleccionará al azar 10 usuarios que necesitan tu ayuda.\n' +
+            'Haz click en el link de paypal de cada usuario y envia dinero. Cualquier cantidad es bienvenida."',
+        mobile: 'Esta es un herramienta mobile. Participa desde tu teléfono.'
+    },
+    distribute: {
+        contribute: 'Contribuí.',
+        main: 'Leveler es una base de datos de trabajadorxs independientes que se quedaron sin ingresos a causa de la pandemia. ',
+        text: 'Los siguientes usuarios han sido seleccionados de manera aleatoria. Procura mandar la misma cantidad a todos. ',
+        thanks: '¡Gracias!'
+    },
+    contribute: {
+        header: '¿Cómo puedo ayudar?',
+        text: 'El equipo de leveler está formado por voluntarios. No tenemos fondos y estamos organizando comunidades dentro y fuera de\n' +
+            'Estados Unidos. Si te gusta el proyecto y te gustaría ayudarnos haz click en el icono de paypal.',
+    },
+    guidelines: {
+        header: 'Instrucciones',
+        important: 'Llena la forma sólo si has sido directamente impactadx económicamente por el COVID-19.',
+        listSecond: 'Si ya recibiste dinero suficiente por favor escríbenos para hacérnoslo saber y quitarte de la lista. Nuestro contacto \n' +
+            'es mx.leveler@gmail.com',
+        listThird: 'Si recibiste más dinero de lo que necesitas, redistribúyelo.',
+        legend: '* Los campos con asterisco serán públicos.'
+    }
+};
