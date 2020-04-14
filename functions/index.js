18f337e1b007943ab206974b07daf5202ee0d79e
Michael Beigelmacher <brooklynbagel@gmail.com>
2020-04-13 21:31:18 -0400

    Implement Cloud Function for daily DB exportsCopy skeleton code for scheduled exportsSchedule exports for productionAdd Firestore library to pkgsFix typoAdd timestamp to bucket pathFix yet another typo

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const firestore = require('@google-cloud/firestore');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const client = new firestore.v1.FirestoreAdminClient();
+
+const now = new Date();
+const bucket = `gs://leveler-8fcc7.appspot.com/firestore_backups/${now.toISOString()}`;
+
+exports.scheduledFirestoreExport = functions.pubsub.schedule('every 24 hours').onRun((_ctx) => {
+  const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+  const databaseName = client.databasePath(projectId, '(default)');
+
+  return client.exportDocuments({
+    name: databaseName,
+    outputUriPrefix: bucket,
+    collectionIds: []
+  })
+  .then(responses => {
+    const response = responses[0];
+    console.log(`Operation Name: ${response['name']}`);
+    return response;
+  })
+  .catch(err => {
+    console.error(err);
+    throw new Error('Export operation failed');
+  });
+});
