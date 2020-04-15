bddadd6c75c5336dfb42497631e7c71b7344bf76
Sam <sambyoung@gmail.com>
2020-04-15 17:45:23 -0400

    testing getRandome()

--- src/components/Distribute/index.js
+++ src/components/Distribute/index.js
@@ -91,14 +91,29 @@ class DistributeTableBase extends Component {
 								entries,
 								loading: false
 							})
+							this.getRandom(entries, 10)
 						}
 					})
 			} catch(e) {
 				console.log(e.message)
 			}
 		}
-
 	}
+
+	getRandom(arr, n) {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+		}
+		console.log(result)
+    return result;
+}
 		
 
 
