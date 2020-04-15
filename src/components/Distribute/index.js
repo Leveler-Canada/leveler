c2cc707257193f2d6ebff71be76da6db8c2c7df4
Sam <sambyoung@gmail.com>
2020-04-15 12:57:32 -0400

    testing query in staging

--- src/components/Distribute/index.js
+++ src/components/Distribute/index.js
@@ -34,6 +34,7 @@ class DistributeTableBase extends Component {
 		try {
 			await entriesCollection
 				.where("random", ">=", random)
+				.where("location.country", "==", "USA")
 				.orderBy("random")
 				.limit(10)
 				.get()
