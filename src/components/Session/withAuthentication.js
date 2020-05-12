70a849d4b0546e2c75489de752aa7a64d04a73ba
Code Bling <codeblingx@gmail.com>
2020-05-12 16:49:45 -0400

    Simplify code massively by using Firebase auth directly

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,57 +1,22 @@
 import React from 'react';
 import AuthUserContext from './context';
-import { withFirebase } from '../Firebase';
+import { withFirebase, default as firebase } from '../Firebase';
 
-const withAuthentication = (Component) => {
-  class WithAuthentication extends React.Component {
-    state = {
-      authUser: null,
-      userData: null
-    }
+const getUserData = async (authUser) => {
+  const { userCollection } = this.props.firebase;
+  try {
+    const doc = await userCollection.doc(authUser.uid).get()
+    const user = doc.data()
 
-    async componentDidMount() {
-      const { auth } = this.props.firebase;
-
-      this.listener = auth.onAuthStateChanged(
-        (authUser) => {
-          authUser
-            ? this.setState({ authUser })
-            : this.setState({ authUser: null, userData: null });
-          if (authUser !== null) {
-            this.getUserData(authUser)
-          }
-        },
-      );
-    }
-
-    componentWillUnmount() {
-      this.listener();
-    }
-
-    async getUserData(authUser) {
-      const { userCollection } = this.props.firebase;
-      try {
-        const doc = await userCollection.doc(authUser.uid).get()
-        const user = doc.data()
-        if (user) {
-          this.setState({
-            userData: user
-          })
-        }
-      } catch (e) {
-        console.log(e.message)
-      }
-    }
-
-    render() {
-      const Inner = () => <Component {...this.props} authUser={this.state.authUser} userData={this.state.userData} />;
-      return (
-        <AuthUserContext.Provider value={this.state.authUser}>
-          <Inner />
-        </AuthUserContext.Provider>
-      );
-    }
+    return user;
+  } catch (e) {
+    console.log(e.message)
   }
-  return withFirebase(WithAuthentication);
 };
+
+const withAuthentication = (Component) => (props) => {
+  const authUser = firebase.auth().currentUser;
+  return withFirebase(<Component {...props} authUser={authUser} userData={async () => await getUserData(authUser)} />);
+};
+
 export default withAuthentication;
