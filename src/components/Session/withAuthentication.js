5849590beb27ad6594ea7e5bd4bd095241be9070
Code Bling <codeblingx@gmail.com>
2020-05-12 17:46:02 -0400

    Return to using state for userData

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -2,21 +2,24 @@ import React from 'react';
 import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
 
-const getUserData = async (firebase, authUser) => {
+const setUserDataState = async (firebase, authUser, setUserData) => {
   const { userCollection } = firebase;
-  try {
-    const doc = await userCollection.doc(authUser.uid).get()
-    const user = doc.data()
+  if ( authUser != null) {
+    try {
+      const doc = await userCollection.doc(authUser.uid).get()
+      const user = doc.data()
 
-    return user;
-  } catch (e) {
-    console.log(e.message)
+      return user;
+    } catch (e) {
+      console.log(e.message)
+    }
   }
 };
 
 const withAuthentication = (Component) => withFirebase((props) => {
+  const [userData, setUserData] = React.useState(null);
   const authUser = props.firebase.auth.currentUser;
-  const userData = authUser ? getUserData(props.firebase, authUser) : null;
+  setUserDataState(props.firebase, authUser, setUserData);
   return <Component {...props} authUser={authUser} userData={userData} />;
 });
 
