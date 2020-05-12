2dde684fa5ae4be9ab2cb413b690ef0ce2cf801e
Code Bling <codeblingx@gmail.com>
2020-05-12 13:01:20 -0400

    Fix spacing in withAuthentication

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -4,50 +4,50 @@ import { withFirebase } from '../Firebase';
 
 const withAuthentication = (Component) => {
   class WithAuthentication extends React.Component {
-		state = {
+    state = {
       authUser: null,
       userData: null
-		}
+    }
 
     async componentDidMount() {
-		  const { auth } = this.props.firebase;
-      
+      const { auth } = this.props.firebase;
+
       this.listener = auth.onAuthStateChanged(
         (authUser) => {
           authUser
             ? this.setState({ authUser })
             : this.setState({ authUser: null, userData: null });
-            if (authUser !== null) {
-              this.getUserData(authUser)
-            }
+          if (authUser !== null) {
+            this.getUserData(authUser)
+          }
         },
       );
     }
 
     componentWillUnmount() {
       this.listener();
-  }
+    }
 
     async getUserData(authUser) {
       const { userCollection } = this.props.firebase;
-        try {
-          const doc = await userCollection.doc(authUser.uid).get()
-          const user = doc.data()
-          if (user) {
-            this.setState({
-              userData: user
-            })
-          }
-        } catch(e) {
-          console.log(e.message)
+      try {
+        const doc = await userCollection.doc(authUser.uid).get()
+        const user = doc.data()
+        if (user) {
+          this.setState({
+            userData: user
+          })
         }
+      } catch (e) {
+        console.log(e.message)
       }
-      
+    }
+
     render() {
-      const Inner = () => <Component {...this.props} authUser={this.state.authUser} userData={this.state.userData}/>;
+      const Inner = () => <Component {...this.props} authUser={this.state.authUser} userData={this.state.userData} />;
       return (
         <AuthUserContext.Provider value={this.state.authUser}>
-          <Inner/>
+          <Inner />
         </AuthUserContext.Provider>
       );
     }
