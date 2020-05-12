7bf54ebc96aa51864a1238817865e2d8c424953a
Code Bling <codeblingx@gmail.com>
2020-05-12 17:14:37 -0400

    Use Firebase from props

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
-import firebase from 'firebase/app';
 
 const getUserData = async (authUser) => {
   const { userCollection } = this.props.firebase;
@@ -16,7 +15,7 @@ const getUserData = async (authUser) => {
 };
 
 const withAuthentication = (Component) => (props) => {
-  const authUser = firebase.auth().currentUser;
+  const authUser = props.firebase.auth().currentUser;
   return withFirebase(<Component {...props} authUser={authUser} userData={async () => await getUserData(authUser)} />);
 };
 
