9f4fbf3b533638a3b64ce5e0eaa0c7a554304fb6
Code Bling <codeblingx@gmail.com>
2020-05-13 18:41:27 -0400

    Wrap returned component in withFirebase for backwards-compatibility

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import AuthUserContext from './context';
+import { withFirebase } from '../Firebase';
 
-const withAuthentication = (Component) => props => {
+const withAuthentication = (Component) => withFirebase(props => {
   const UserContext = React.useContext(AuthUserContext);
 
   return <Component {...props} {...UserContext} />;
-};
+});
 export default withAuthentication;
